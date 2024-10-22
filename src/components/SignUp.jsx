35544596e071da415db11aa84034e4039e696eaf
muzamilmemon1 <muhammadmuzamilhussain5@gmail.com>
2024-10-22 10:29:36 +0500

    Temporarily commented out age field in signup form for regression testing

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -65,7 +65,7 @@ function SignUp() {
           {({ isSubmitting }) => (
             <Form className="space-y-6">
               <div>
-                <label  
+                <label
                   htmlFor="name"
                   className="block text-sm font-medium leading-6 text-gray-900 text-left"
                 >
@@ -86,7 +86,7 @@ function SignUp() {
                   />
                 </div>
               </div>
-              <div>
+              {/* <div>
                 <label
                   htmlFor="age"
                   className="block text-sm font-medium leading-6 text-gray-900 text-left"
@@ -107,7 +107,7 @@ function SignUp() {
                     className="text-red-500 text-sm"
                   />
                 </div>
-              </div>
+              </div> */}
               <div>
                 <label
                   htmlFor="email"
