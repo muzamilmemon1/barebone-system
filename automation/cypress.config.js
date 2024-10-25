f6f95238ae4b45efcb1f822cc5f5a937ea392aba
muzamilmemon1 <muhammadmuzamilhussain5@gmail.com>
2024-10-25 15:25:44 +0500

    change relative path to absolute path and switch yarn to npm

--- automation/cypress.config.js
+++ automation/cypress.config.js
@@ -3,8 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    supportFile: "./cypress/support/e2e.js",
-    specPattern: "./cypress/e2e/**/*.spec.cy.{js,jsx,ts,tsx}",
+    supportFile:
+      "/home/muzamil/Desktop/barebone-system/automation/cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*.spec.cy.{js,jsx,ts,tsx}",
   },
 
   component: {
